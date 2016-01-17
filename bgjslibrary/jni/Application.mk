91d3d61b513d3c55ec6bf5f4a96914b161e3d303
Kevin Read <me@kevin-read.com>
2016-01-17 17:35:02 +0100

    Update to new android ndk, api to 14, clang to 3.6

--- bgjslibrary/jni/Application.mk
+++ bgjslibrary/jni/Application.mk
@@ -1,7 +1,7 @@
 APP_ABI := x86 armeabi-v7a
 APP_STL := stlport_static
-APP_PLATFORM := android-9
+APP_PLATFORM := android-14
 
-NDK_TOOLCHAIN_VERSION=clang3.4
+NDK_TOOLCHAIN_VERSION=clang3.6
 
 #APP_OPTIM := debug
