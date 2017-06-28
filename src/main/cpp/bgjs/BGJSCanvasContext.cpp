c2cf1995adafe4b9323f7d4082be886e53e759af
Kevin Read <me@kevin-read.com>
2017-06-28 17:03:57 +0200

    JNI code doesn't set clearColor anymore statically to black. use setClearColor on V8TextureView to set it.

--- src/main/cpp/bgjs/BGJSCanvasContext.cpp
+++ src/main/cpp/bgjs/BGJSCanvasContext.cpp
@@ -153,7 +153,6 @@ void BGJSCanvasContext::resize (int widthp, int heightp, bool resizeOnly) {
 	}
 #endif
 
-     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     checkGlError("glClear(resize)");
 
