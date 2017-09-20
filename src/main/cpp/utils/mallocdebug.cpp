3a37a182c7e92856599ca8ddb7773cd3bd367a24
Kevin Read <kread@om7sense.com>
2017-09-20 12:27:41 +0200

    Update build for new gradle syntax. Filter out arm64 for now. 64bit compile fixes. Build type debugging.

--- src/main/cpp/utils/mallocdebug.cpp
+++ src/main/cpp/utils/mallocdebug.cpp
@@ -8,13 +8,13 @@
 
 void* myMalloc (size_t size, const char* file, int line, const char* func) {
 	void* ret = malloc(size);
-	LOGD("malloc of %u = %p in %s line %d file %s", size, ret, func, line, file);
+	LOGD("malloc of %zu = %p in %s line %d file %s", size, ret, func, line, file);
 	return ret;
 }
 
 void* myRealloc (void* ptr, size_t size, const char* file, int line, const char* func) {
 	void* ret = realloc(ptr, size);
-	LOGD("realloc of %u, %p = %p in %s line %d file %s", size, ptr, ret, func, line, file);
+	LOGD("realloc of %zu, %p = %p in %s line %d file %s", size, ptr, ret, func, line, file);
 	return ret;
 }
 
@@ -24,7 +24,7 @@ void myFree(void* ptr, const char* file, int line, const char* func) {
 }
 
 void* myMemset (void* ptr, int value, size_t num, const char* file, int line, const char* func) {
-	LOGD("memset of %d bytes at %p with %i in %s line %d file %s", num, ptr, value, func, line, file);
+	LOGD("memset of %zu bytes at %p with %i in %s line %d file %s", num, ptr, value, func, line, file);
 	return memset (ptr, value, num);
 }
 
@@ -35,11 +35,11 @@ char* myStrDup (const char* str, const char* file, int line, const char* func) {
 }
 
 void* myMemcpy ( void * destination, const void * source, size_t num, const char* file, int line, const char* func) {
-	LOGD("memcpy by %u from %p - %p to %p - %p in %s line %d file %s", num, source, (char*)source + num, destination, (char*)destination + num, func, line, file);
+	LOGD("memcpy by %zu from %p - %p to %p - %p in %s line %d file %s", num, source, (char*)source + num, destination, (char*)destination + num, func, line, file);
 	return memcpy(destination, source, num);
 }
 
 void myBcopy ( const void *src, const void *dest, size_t num, const char* file, int line, const char* func) {
-	LOGD("memcpy by %u from %p - %p to %p - %p in %s line %d file %s", num, src, (char*)src + num, dest, (char*)dest + num, func, line, file);
+	LOGD("memcpy by %zu from %p - %p to %p - %p in %s line %d file %s", num, src, (char*)src + num, dest, (char*)dest + num, func, line, file);
 	// bcopy (src, dest, num);
 }
