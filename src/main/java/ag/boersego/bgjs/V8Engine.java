13f833b379a039339a12985563e1ea4ad6c7372a
Tim Pohrer <tim.pohrer@boerse-go.de>
2017-10-13 10:09:19 +0200

    [MYR-2024]: UnsatisfiedLinkError

--- src/main/java/ag/boersego/bgjs/V8Engine.java
+++ src/main/java/ag/boersego/bgjs/V8Engine.java
@@ -224,8 +224,19 @@ public class V8Engine extends Thread implements Handler.Callback {
 	@Override
 	public void run() {
 		this.setName("V8Engine");
-		
-		System.loadLibrary("bgjs");
+
+		try {
+            System.loadLibrary("bgjs");
+        } catch (UnsatisfiedLinkError e){
+
+		    if(DEBUG){
+		        Log.e(TAG,"UnsatisfiedLinkError while loading library \"bgjs\"");
+                e.printStackTrace();
+		    }
+		    
+            return;
+
+        }
 
 		Looper.prepare();
 		mHandler = new Handler (this);
