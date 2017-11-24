f4607ebd8a32b0cd0ed92cf9400da34b2e6b1d21
Kevin Read <me@kevin-read.com>
2017-11-24 16:38:45 +0100

    Drop empty websocket request kotlin unit

--- src/main/java/ag/boersego/bgjs/modules/BGJSModuleWebSocketRequest.kt
+++
@@ -1,6 +0,0 @@
-package ag.boersego.bgjs.modules
-
-/**
- * Created by Kevin Read <me@kevin-read.com> on 24.11.17 for myrmecophaga-2.0.
- * Copyright (c) 2017 ${ORGANIZATION_NAME}. All rights reserved.
- */
