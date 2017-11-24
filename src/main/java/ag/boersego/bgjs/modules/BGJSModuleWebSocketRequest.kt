27f149f6fb19f260a3bdd4ab4f8a3e914667c4c5
Kevin Read <me@kevin-read.com>
2017-11-24 14:16:40 +0100

    MYR-2279 Dependency: Web socket module

---
+++ src/main/java/ag/boersego/bgjs/modules/BGJSModuleWebSocketRequest.kt
@@ -0,0 +1,6 @@
+package ag.boersego.bgjs.modules
+
+/**
+ * Created by Kevin Read <me@kevin-read.com> on 24.11.17 for myrmecophaga-2.0.
+ * Copyright (c) 2017 ${ORGANIZATION_NAME}. All rights reserved.
+ */
