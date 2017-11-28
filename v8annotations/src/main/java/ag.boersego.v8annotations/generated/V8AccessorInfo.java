cc9d5115c6aaa7fa72d1e1c9e89d304c125f030f
Kevin Read <me@kevin-read.com>
2017-11-28 15:49:12 +0100

    Add type to accessor info

--- v8annotations/src/main/java/ag.boersego.v8annotations/generated/V8AccessorInfo.java
+++ v8annotations/src/main/java/ag.boersego.v8annotations/generated/V8AccessorInfo.java
@@ -8,12 +8,14 @@ public class V8AccessorInfo {
     public String property;
     public String setter;
     public String getter;
+    public String type;
     public boolean isStatic;
 
-    public V8AccessorInfo(String property, String getter, String setter, boolean isStatic) {
+    public V8AccessorInfo(String property, String type, String getter, String setter, boolean isStatic) {
         this.property = property;
         this.getter = getter;
         this.setter = setter;
+        this.type = type;
         this.isStatic = isStatic;
     };
 };
