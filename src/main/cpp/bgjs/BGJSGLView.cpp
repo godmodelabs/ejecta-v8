e38f686a67c2a7ec08e7f6097fe4c4d4f989d89d
Kevin Read <me@kevin-read.com>
2018-03-02 18:15:38 +0100

    Rewire most of BGJSGLView, needs testing once init works

--- src/main/cpp/bgjs/BGJSGLView.cpp
+++ src/main/cpp/bgjs/BGJSGLView.cpp
@@ -47,7 +47,6 @@ static void checkGlError(const char* op) {
 BGJS_JNI_LINK(BGJSGLView, "ag/boersego/bgjs/BGJSGLView");
 
 void BGJSGLView::initializeJNIBindings(JNIClassInfo *info, bool isReload) {
-	info->registerNativeMethod("Create", "(Lag/boersego/bgjs/V8Engine)Lag/boersego/bgjs/JNIV8GenericObject;", (void*)BGJSGLView::jniCreate);
     info->registerNativeMethod("prepareRedraw", "()V", (void*)BGJSGLView::prepareRedraw);
     info->registerNativeMethod("endRedraw", "()V", (void*)BGJSGLView::endRedraw);
     info->registerNativeMethod("setTouchPosition", "(II)V", (void*)BGJSGLView::setTouchPosition);
@@ -58,24 +57,8 @@ void BGJSGLView::initializeV8Bindings(JNIV8ClassInfo *info) {
 
 }
 
-jobject BGJSGLView::jniCreate(JNIEnv *env, jobject obj, jobject engineObj) {
-	auto engine = JNIWrapper::wrapObject<BGJSV8Engine>(engineObj);
-
-	v8::Isolate* isolate = engine->getIsolate();
-	v8::Locker l(isolate);
-	v8::Isolate::Scope isolateScope(isolate);
-	v8::HandleScope scope(isolate);
-	v8::Local<v8::Context> context = engine->getContext();
-	v8::Context::Scope ctxScope(context);
-
-	v8::Local<v8::Object> objRef;
-
-	objRef = v8::Object::New(isolate);
-
-	return JNIV8Wrapper::wrapObject<BGJSGLView>(objRef)->getJObject();
-}
-
 void BGJSGLView::setViewData(float pixelRatio, bool doNoClearOnFlip, int width, int height) {
+    auto engine = JNIWrapper::wrapObject<BGJSV8Engine>(engineObj);
 
 	noFlushOnRedraw = false;
     noClearOnFlip = doNoClearOnFlip;
