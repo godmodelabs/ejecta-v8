186b039c54897507a34f443a1ac91ad6c1304080
Mark Plomer <mark.plomer@boerse-go.de>
2022-01-24 11:48:03 +0100

    implemented lock-monitoring (callContext) for _callAsV8Function in JAVA

--- src/main/java/ag/boersego/bgjs/JNIV8Function.java
+++ src/main/java/ag/boersego/bgjs/JNIV8Function.java
@@ -20,81 +20,89 @@ final public class JNIV8Function extends JNIV8Object {
 
     public @Nullable
     Object callAsV8Function(@Nullable Object... arguments) {
-        return _callAsV8Function(false, 0, 0, Object.class, null, arguments);
+        String callContext = "callAsV8Function";
+
+        /*
+        // enable for debugging locks (disabled by default for performance reasons):
+        StackTraceElement element = Thread.currentThread().getStackTrace()[3];
+        callContext += "/" + element.getClassName() + "::" + element.getMethodName();
+        */
+
+        return _callAsV8Function(false, 0, 0, Object.class, null, callContext, arguments);
     }
 
     public @Nullable
     Object applyAsV8Function(@NonNull Object[] arguments) {
-        return _callAsV8Function(false, 0, 0, Object.class, null, arguments);
+        return _callAsV8Function(false, 0, 0, Object.class, null, "applyAsV8Function", arguments);
     }
 
     public @Nullable
     Object callAsV8FunctionWithReceiver(@NonNull Object receiver, @Nullable Object... arguments) {
-        return _callAsV8Function(false, 0, 0, Object.class, receiver, arguments);
+        return _callAsV8Function(false, 0, 0, Object.class, receiver, "callAsV8FunctionWithReceiver", arguments);
     }
 
     public @Nullable
     Object applyAsV8FunctionWithReceiver(@NonNull Object receiver, @NonNull Object[] arguments) {
-        return _callAsV8Function(false, 0, 0, Object.class, receiver, arguments);
+        return _callAsV8Function(false, 0, 0, Object.class, receiver, "applyAsV8FunctionWithReceiver", arguments);
     }
 
     public @NonNull
     Object callAsV8Constructor(@Nullable Object... arguments) {
-        return _callAsV8Function(true, V8Flags.NonNull, 0, Object.class, null, arguments);
+        return _callAsV8Function(true, V8Flags.NonNull, 0, Object.class, null, "callAsV8Constructor", arguments);
     }
 
     public @NonNull
     Object applyAsV8Constructor(@Nullable Object[] arguments) {
-        return _callAsV8Function(true, V8Flags.NonNull, 0, Object.class, null, arguments);
+        return _callAsV8Function(true, V8Flags.NonNull, 0, Object.class, null, "applyAsV8Constructor", arguments);
     }
 
 
     @SuppressWarnings("unchecked")
     public @Nullable
     <T> T callAsV8FunctionTyped(int flags, @NonNull Class<T> returnType, @Nullable Object... arguments) {
-        return (T) _callAsV8Function(false, flags, returnType.hashCode(), returnType, null, arguments);
+        return (T) _callAsV8Function(false, flags, returnType.hashCode(), returnType, null, "callAsV8FunctionTyped", arguments);
     }
 
     @SuppressWarnings("unchecked")
     public @Nullable
     <T> T callAsV8FunctionTyped(@NonNull Class<T> returnType, @Nullable Object... arguments) {
-        return (T) _callAsV8Function(false, V8Flags.Default, returnType.hashCode(), returnType, null, arguments);
+        return (T) _callAsV8Function(false, V8Flags.Default, returnType.hashCode(), returnType, null, "callAsV8FunctionTyped", arguments);
     }
 
     @SuppressWarnings("unchecked")
     public @Nullable
     <T> T callAsV8FunctionWithReceiverTyped(int flags, @NonNull Class<T> returnType, @NonNull Object receiver, @Nullable Object... arguments) {
-        return (T) _callAsV8Function(false, flags, returnType.hashCode(), returnType, receiver, arguments);
+        return (T) _callAsV8Function(false, flags, returnType.hashCode(), returnType, receiver, "callAsV8FunctionWithReceiverTyped", arguments);
     }
 
     @SuppressWarnings("unchecked")
     public @Nullable
     <T> T callAsV8FunctionWithReceiverTyped(@NonNull Class<T> returnType, @NonNull Object receiver, @Nullable Object... arguments) {
-        return (T) _callAsV8Function(false, V8Flags.Default, returnType.hashCode(), returnType, receiver, arguments);
+        return (T) _callAsV8Function(false, V8Flags.Default, returnType.hashCode(), returnType, receiver, "callAsV8FunctionWithReceiverTyped", arguments);
     }
 
     @SuppressWarnings("unchecked")
     public @Nullable
     <T> T applyAsV8FunctionTyped(int flags, @NonNull Class<T> returnType, @NonNull Object[] arguments) {
-        return (T) _callAsV8Function(false, flags, returnType.hashCode(), returnType, null, arguments);
+        return (T) _callAsV8Function(false, flags, returnType.hashCode(), returnType, null, "applyAsV8FunctionTyped", arguments);
     }
 
     @SuppressWarnings("unchecked")
     public @Nullable
     <T> T applyAsV8FunctionTyped(@NonNull Class<T> returnType, @NonNull Object[] arguments) {
-        return (T) _callAsV8Function(false, V8Flags.Default, returnType.hashCode(), returnType, null, arguments);
+        return (T) _callAsV8Function(false, V8Flags.Default, returnType.hashCode(), returnType, null, "applyAsV8FunctionTyped", arguments);
     }
 
     @SuppressWarnings("unchecked")
     public @Nullable
     <T> T applyAsV8FunctionWithReceiverTyped(int flags, @NonNull Class<T> returnType, @NonNull Object receiver, @NonNull Object[] arguments) {
-        return (T) _callAsV8Function(false, flags, returnType.hashCode(), returnType, receiver, arguments);
+        return (T) _callAsV8Function(false, flags, returnType.hashCode(), returnType, receiver, "applyAsV8FunctionWithReceiverTyped", arguments);
     }
 
     @SuppressWarnings("unchecked")
     public @Nullable
     <T> T applyAsV8FunctionWithReceiverTyped(@NonNull Class<T> returnType, @NonNull Object receiver, @NonNull Object[] arguments) {
-        return (T) _callAsV8Function(false, V8Flags.Default, returnType.hashCode(), returnType, receiver, arguments);
+        return (T) _callAsV8Function(false, V8Flags.Default, returnType.hashCode(), returnType, receiver, "applyAsV8FunctionWithReceiverTyped", arguments);
     }
 
     @Override
@@ -104,7 +112,7 @@ final public class JNIV8Function extends JNIV8Object {
 
     //------------------------------------------------------------------------
     // internal fields & methods
-    private native Object _callAsV8Function(boolean asConstructor, int flags, int type, Class returnType, Object receiver, Object... arguments);
+    private native Object _callAsV8Function(boolean asConstructor, int flags, int type, Class returnType, Object receiver, String callContext, Object... arguments);
 
     @Keep
     protected JNIV8Function(V8Engine engine, long jsObjPtr, Object[] arguments) {
